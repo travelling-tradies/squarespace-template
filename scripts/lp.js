8717d1162610afaaa87fc772791baa1d4f4ada6e
Kody Deretic <kodyderetic@hotmail.com>
2018-07-11 12:43:03 +1000

    small js and css fixes

--- scripts/lp.js
+++ scripts/lp.js
@@ -1,27 +1,36 @@
 
-$("#block-yui_3_17_2_1_1530853676327_8650").appendTo(".su-form-tradie");
-$("#block-yui_3_17_2_1_1530857589866_35655").appendTo(".su-form-host");
+$(document).ready(function() {
 
-  $('.su-exit').click(function() {
-    $('#signup-opacity').removeClass('suo-active');
-    $('#signup-outer').removeClass('su-active');
+  var signupIsActiveClass = 'su-active';
+  var signupFormIsActiveClass = 'su-form-active';
+  var signupFormOpacityIsActiveClass = 'suo-active';
+  var signupFormToggleIsNotActiveClass = 'su-toggle-not-active';
+
+  $("#block-yui_3_17_2_1_1530853676327_8650").appendTo(".su-form-tradie");
+  $("#block-yui_3_17_2_1_1530857589866_35655").appendTo(".su-form-host");
+
+  $(document).on('click', '.su-exit', function(){
+    $('#signup-opacity').removeClass(signupFormOpacityIsActiveClass);
+    $('#signup-outer').removeClass(signupIsActiveClass);
   })
 
-	$('.lp-signup-open').click(function() {
-		$('#signup-opacity').addClass('suo-active');
-		$('#signup-outer').addClass('su-active');
-	})
-
-  $('.su-toggle-host').click(function() {
-    $('.su-toggle-host').removeClass('su-toggle-not-active');
-    $('.su-toggle-tradie').addClass('su-toggle-not-active');
-    $('.su-form-host').addClass('su-form-active');
-    $('.su-form-tradie').removeClass('su-form-active');
+  $(document).on('click', '.lp-signup-open', function(){
+    $('#signup-opacity').addClass(signupFormOpacityIsActiveClass);
+    $('#signup-outer').addClass(signupIsActiveClass);
   })
 
-  $('.su-toggle-tradie').click(function() {
-    $('.su-toggle-host').addClass('su-toggle-not-active');
-    $('.su-toggle-tradie').removeClass('su-toggle-not-active');
-    $('.su-form-host').removeClass('su-form-active');
-    $('.su-form-tradie').addClass('su-form-active');
+  $(document).on('click', '.su-toggle-host', function(){
+    $('.su-toggle-host').removeClass(signupFormToggleIsNotActiveClass);
+    $('.su-toggle-tradie').addClass(signupFormToggleIsNotActiveClass);
+    $('.su-form-host').addClass(signupFormIsActiveClass);
+    $('.su-form-tradie').removeClass(signupFormIsActiveClass);
   })
+
+  $(document).on('click', '.su-toggle-tradie', function(){
+    $('.su-toggle-host', '.su-toggle-tradie').addClass(signupFormToggleIsNotActiveClass);
+    $('.su-toggle-tradie').removeClass(signupFormToggleIsNotActiveClass);
+    $('.su-form-host').removeClass(signupFormIsActiveClass);
+    $('.su-form-tradie').addClass(signupFormIsActiveClass);
+  })
+
+});
